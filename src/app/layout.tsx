d285fde37315291fa23f36cbc4e10b56ef4d0812
ali.hamdan <ali.hamdan@wonderdays.com>
2024-04-11 20:57:22 +0400

    initialize project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={cn("h-full relative font-sans antialiased", inter.className)}>
+        <main className="min-h-screen relative flex flex-col">
+          <div className="flex-grow flex-1">{children}</div>
+        </main>
+      </body>
     </html>
   );
 }
