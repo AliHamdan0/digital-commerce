4c0ee5491997c38c20c10a5d4d77e315d43bab79
ali.hamdan <ali.hamdan@wonderdays.com>
2024-05-11 12:38:26 +0400

    finalizing

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: "http",
-        hostname: "localhost",
-      },
-      {
-        protocol: "https",
-        hostname: "digital-commerce.up.railway.app",
-      },
-    ],
+    domains: ["digital-commerce.up.railway.app", "localhost"],
   },
 };
 
