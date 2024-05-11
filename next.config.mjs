6046e19ef71ce9d5918a11953237b8baae826606
ali.hamdan <ali.hamdan@wonderdays.com>
2024-05-11 12:15:26 +0400

    finalizing

--- next.config.mjs
+++ next.config.mjs
@@ -8,7 +8,7 @@ const nextConfig = {
       },
       {
         protocol: "https",
-        hostname: "digitalhippo-production.up.railway.app",
+        hostname: "digital-commerce.up.railway.app",
       },
     ],
   },
