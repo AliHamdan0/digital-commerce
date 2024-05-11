f06821f786452c657c2ebdff7a66beab2f6ea681
ali.hamdan <ali.hamdan@wonderdays.com>
2024-05-11 11:41:28 +0400

    updating types

--- src/collections/Products.ts
+++ src/collections/Products.ts
@@ -16,50 +16,50 @@ const syncUser: AfterChangeHook<Product> = async ({ req, doc }) => {
     id: req.user.id,
   });
 
-  // if (fullUser && typeof fullUser === "object") {
-  //   const { products } = fullUser;
+  if (fullUser && typeof fullUser === "object") {
+    const { products } = fullUser;
 
-  //   const allIDs = [...(products?.map((product) => (typeof product === "object" ? product.id : product)) || [])];
+    const allIDs = [...(products?.map((product) => (typeof product === "object" ? product.id : product)) || [])];
 
-  //   const createdProductIDs = allIDs.filter((id, index) => allIDs.indexOf(id) === index);
+    const createdProductIDs = allIDs.filter((id, index) => allIDs.indexOf(id) === index);
 
-  //   const dataToUpdate = [...createdProductIDs, doc.id];
+    const dataToUpdate = [...createdProductIDs, doc.id];
 
-  //   await req.payload.update({
-  //     collection: "users",
-  //     id: fullUser.id,
-  //     data: {
-  //       products: dataToUpdate,
-  //     },
-  //   });
-  // }
+    await req.payload.update({
+      collection: "users",
+      id: fullUser.id,
+      data: {
+        products: dataToUpdate,
+      },
+    });
+  }
 };
 
-// const isAdminOrHasAccess =
-//   (): Access =>
-//   ({ req: { user: _user } }) => {
-//     const user = _user as User | undefined;
+const isAdminOrHasAccess =
+  (): Access =>
+  ({ req: { user: _user } }) => {
+    const user = _user as User | undefined;
 
-//     if (!user) return false;
-//     if (user.role === "admin") return true;
+    if (!user) return false;
+    if (user.role === "admin") return true;
 
-// const userProductIDs = (user.products || []).reduce<Array<string>>((acc, product) => {
-//   if (!product) return acc;
-//   if (typeof product === "string") {
-//     acc.push(product);
-//   } else {
-//     acc.push(product.id);
-//   }
+    const userProductIDs = (user.products || []).reduce<Array<string>>((acc, product) => {
+      if (!product) return acc;
+      if (typeof product === "string") {
+        acc.push(product);
+      } else {
+        acc.push(product.id);
+      }
 
-//   return acc;
-// }, []);
+      return acc;
+    }, []);
 
-// return {
-//   id: {
-//     in: userProductIDs,
-//   },
-// };
-// };
+    return {
+      id: {
+        in: userProductIDs,
+      },
+    };
+  };
 
 export const Products: CollectionConfig = {
   slug: "products",
@@ -67,9 +67,9 @@ export const Products: CollectionConfig = {
     useAsTitle: "name",
   },
   access: {
-    read: () => true,
-    update: () => true,
-    delete: () => true,
+    read: isAdminOrHasAccess(),
+    update: isAdminOrHasAccess(),
+    delete: isAdminOrHasAccess(),
   },
   hooks: {
     afterChange: [syncUser],
--- src/collections/Users.ts
+++ src/collections/Users.ts
@@ -40,26 +40,26 @@ export const Users: CollectionConfig = {
     defaultColumns: ["id"],
   },
   fields: [
-    //   {
-    //     name: "products",
-    //     label: "Products",
-    //     admin: {
-    //       condition: () => false,
-    //     },
-    //     type: "relationship",
-    //     relationTo: "products",
-    //     hasMany: true,
-    //   },
-    //   {
-    //     name: "product_files",
-    //     label: "Product files",
-    //     admin: {
-    //       condition: () => false,
-    //     },
-    //     type: "relationship",
-    //     relationTo: "product_files",
-    //     hasMany: true,
-    //   },
+    {
+      name: "products",
+      label: "Products",
+      admin: {
+        condition: () => false,
+      },
+      type: "relationship",
+      relationTo: "products",
+      hasMany: true,
+    },
+    {
+      name: "product_files",
+      label: "Product files",
+      admin: {
+        condition: () => false,
+      },
+      type: "relationship",
+      relationTo: "product_files",
+      hasMany: true,
+    },
     {
       name: "role",
       defaultValue: "user",
